8f675278a4e6b1f0d883e66a490668509d0b20ce
Juan David Sarmiento Agudelo <juansarmientopo@gmail.com>
2026-02-15 16:10:32 -0500

    Creat blank solution

---
+++ ConsoleApp1/inNumberOddOrNot/Program.cs
@@ -0,0 +1,26 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.Write("Ingrese Un Numero entero o 'S' para salir: ");
+    numberString = Console.ReadLine();
+    if (numberString == "S")
+    {
+        continue;
+    }
+
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberint)) ;
+    {
+        if (numberint % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numberint}, es par");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numberint}, es impar");
+        }
+    else
+    }
+     
+} while (numberString != "S");
+Console.WriteLine("Game Over");
