98a6953dd67fc68b1f67683eaf1879366a37a974
Juan David Sarmiento Agudelo <juansarmientopo@gmail.com>
2026-02-16 06:40:43 -0500

    The grates Number

---
+++ ConsoleApp1/TheGreathestNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    Console.WriteLine("Ingrese 3 números diferentes");
+
+    var a = ConsoleExtencion.GetInt("Ingrese primer número : ");
+    var b = ConsoleExtencion.GetInt("Ingrese segundo número: ");
+    var c = ConsoleExtencion.GetInt("Ingrese tercer número : ");
+
+    if (a > b && a > c)
+    {
+        Console.WriteLine($"El número mayor es: {a}");
+    }
+    else if (b > a && b > c)
+    {
+        Console.WriteLine($"El número mayor es: {b}");
+    }
+    else
+    {
+        Console.WriteLine($"El número mayor es: {c}");
+    }
+
+    do
+    {
+        answer = ConsoleExtencion.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    }
+    while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+}
+while (answer.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
--- ConsoleApp1/inNumberOddOrNot/Program.cs
+++ ConsoleApp1/inNumberOddOrNot/Program.cs
@@ -1,33 +1,25 @@
-﻿var numberString = string.Empty;
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
 
 do
 {
-    Console.Write("Ingrese número entero o la palabra 'salir' para salir: ");
-    numberString = Console.ReadLine();
-
-    if (numberString.ToLower() == "salir")
+    var number = ConsoleExtencion.GetInt("Ingrese número entero: ");
+    if (number % 2 == 0)
     {
-        continue;
-    }
-
-    var numberInt = 0;
-
-    if (int.TryParse(numberString, out numberInt))
-    {
-        if (numberInt % 2 == 0)
-        {
-            Console.WriteLine($"El número {numberInt}, es par.");
-        }
-        else
-        {
-            Console.WriteLine($"El número {numberInt}, es impar.");
-        }
+        Console.WriteLine($"El número {number}, es par.");
     }
     else
     {
-        Console.WriteLine($"Lo que ingresaste: {numberString}, no es un número entero.");
+        Console.WriteLine($"El número {number}, es impar.");
     }
 
-} while (numberString.ToLower() != "salir");
+    do
+    {
+        answer = ConsoleExtencion.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
 
 Console.WriteLine("Game Over.");
