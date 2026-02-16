ada50aaa06e6c8b05bf0cc674def84ee3836ce14
Juan David Sarmiento Agudelo <juansarmientopo@gmail.com>
2026-02-16 07:26:16 -0500

    Hii

---
+++ ConsoleApp1/IsLeapYear/Program.cs
@@ -0,0 +1,47 @@
+﻿using Shared;
+
+do
+{
+    var currentYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = ConsoleExtencion.GetInt("Ingrese año: ");
+
+    if (year == currentYear)
+    {
+        message = "es";
+    }
+    else if (year > currentYear)
+    {
+        message = "va a ser";
+    }
+    else
+    {
+        message = "fue";
+    }
+
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                Console.WriteLine($"El año {year} SI {message} bisiesto.");
+            }
+            else
+            {
+                Console.WriteLine($"El año {year} NO {message} no es bisiesto.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"El año {year} SI {message} bisiesto.");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El año {year} NO {message} no es bisiesto.");
+    }
+}
+while (true);
+
+
